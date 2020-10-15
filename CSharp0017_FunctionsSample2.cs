046d471fbe97106128b550de354794bc5e25c930
Abdullah06444 <abdullah44gur@gmail.com>
2020-10-15 14:12:17 +0300

    initial commit

---
+++ CSharp0017_FunctionsSample2.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace CSharp0017_FunctionsSample2
+{
+    class CSharp0017_FunctionsSample2
+    {
+        static int isPentagonal(int number)
+        {
+            int i;
+            for (i = 0; number > 0; i++)
+                number -= 1 + 3 * i;
+            if (number == 0)
+                return 1;
+            return 0;
+        }
+
+        static void Main(string[] args)
+        {
+
+            int x;
+            do
+            {
+
+                x = int.Parse(Console.ReadLine());
+                if (x > 0)
+                    if(Convert.ToBoolean(isPentagonal(x)))
+                        Console.WriteLine(x + " is a triangular number");
+                    else
+                        Console.WriteLine(x + " is NOT a triangular number");
+            } while (x > 0);
+        }
+    }
+}
